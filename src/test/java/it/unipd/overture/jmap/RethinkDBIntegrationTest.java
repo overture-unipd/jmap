886585663886f77e15f1130a5251b70ddd6983eb
Riccardo Fabbian <fabb.riccardo@gmail.com>
2023-12-29 09:41:18 +0100

    Applicazione formato di EditorConfig a `RethinkDBIntegrationTest.java`

--- src/test/java/it/unipd/overture/jmap/RethinkDBIntegrationTest.java
+++ src/test/java/it/unipd/overture/jmap/RethinkDBIntegrationTest.java
@@ -18,50 +18,51 @@ import com.google.gson.JsonObject;
 
 public class RethinkDBIntegrationTest {
 
-    private static final RethinkDB r = RethinkDB.r;
+  private static final RethinkDB r = RethinkDB.r;
 
-    @Test
-    void testRethinkDBContainer() {
-        try (GenericContainer<?> rethinkDBContainer = new GenericContainer<>("rethinkdb:latest")
-                .withExposedPorts(28015)
-                .waitingFor(Wait.forLogMessage(".*Server ready.*\\n", 1))) {
+  @Test
+  void testRethinkDBContainer() {
+    try (GenericContainer<?> rethinkDBContainer = new GenericContainer<>("rethinkdb:latest")
+        .withExposedPorts(28015)
+        .waitingFor(Wait.forLogMessage(".*Server ready.*\\n", 1))) {
 
-            rethinkDBContainer.start();
+      rethinkDBContainer.start();
 
-            // Retrieve connection details
-            String host = rethinkDBContainer.getContainerIpAddress();
-            Integer port = rethinkDBContainer.getMappedPort(28015);
+      // Retrieve connection details
+      String host = rethinkDBContainer.getContainerIpAddress();
+      Integer port = rethinkDBContainer.getMappedPort(28015);
 
-            try (Connection connection = r.connection().hostname(host).port(port).connect()) {
-                // Use the RethinkDB driver to perform a simple operation
-                r.db("test").tableCreate("test_table").run(connection);
+      try (Connection connection = r.connection().hostname(host).port(port).connect()) {
+        // Use the RethinkDB driver to perform a simple operation
+        r.db("test").tableCreate("test_table").run(connection);
 
-                // Insert a document
-				        var inputDB = "{\"name\":\"John\",\"age\":30}";
-                var insertDB = r.db("test").table("test_table").insert(r.json(inputDB)).toJson().run(connection);
+        // Insert a document
+        var inputDB = "{\"name\":\"John\",\"age\":30}";
+        var insertDB = r.db("test").table("test_table").insert(r.json(inputDB)).toJson().run(connection);
 
-                JsonObject objJson = new Gson().fromJson(insertDB.first().toString(), JsonObject.class);
-                var key = objJson.get("generated_keys").getAsString();
+        JsonObject objJson = new Gson().fromJson(insertDB.first().toString(), JsonObject.class);
+        var key = objJson.get("generated_keys").getAsString();
 
-                // Query the inserted document
-                boolean qTableExists = !r.db("test")
-                                        .table("test_table")
-                                        .get(key)
-                                        .run(connection)
-                                        .<Long>toList()
-                                        .isEmpty();
+        // Query the inserted document
+        boolean qTableExists = !r.db("test")
+            .table("test_table")
+            .get(key)
+            .run(connection)
+            .<Long>toList()
+            .isEmpty();
 
-                // Assert that the document was successfully inserted
-                assertTrue(qTableExists, "Document should exist in the table.");
+        // Assert that the document was successfully inserted
+        assertTrue(qTableExists, "Document should exist in the table.");
 
-                var qTableContent = r.db("test").table("test_table").get(key).without("id").toJson().run(connection).first().toString();
-                JsonObject resultJson = new Gson().fromJson(qTableContent, JsonObject.class);
-                JsonObject inputJson = new Gson().fromJson(inputDB, JsonObject.class);
-                assertEquals(inputJson, resultJson);
-            }
-        } catch (Exception e) {
-            e.printStackTrace(); // Handle exceptions appropriately in your tests
-            fail("Test failed with an exception: " + e.getMessage());
-        }
+        var qTableContent = r.db("test").table("test_table").get(key).without("id").toJson().run(connection)
+            .first().toString();
+        JsonObject resultJson = new Gson().fromJson(qTableContent, JsonObject.class);
+        JsonObject inputJson = new Gson().fromJson(inputDB, JsonObject.class);
+        assertEquals(inputJson, resultJson);
+      }
+    } catch (Exception e) {
+      e.printStackTrace(); // Handle exceptions appropriately in your tests
+      fail("Test failed with an exception: " + e.getMessage());
     }
+  }
 }
