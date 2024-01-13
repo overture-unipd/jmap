e1435fd8155957454906464304281c14dea0b27b
francescobulychov <fc.bulychov@protonmail.com>
2024-01-13 09:53:09 +0100

    Implementazione di Thread/get e Thread/changes

--- src/main/java/it/unipd/overture/jmap/Jmap.java
+++ src/main/java/it/unipd/overture/jmap/Jmap.java
@@ -26,6 +26,7 @@ import com.google.gson.reflect.TypeToken;
 
 import rs.ltt.jmap.common.*;
 import rs.ltt.jmap.common.entity.*;
+import rs.ltt.jmap.common.entity.Thread;
 import rs.ltt.jmap.common.entity.filter.EmailFilterCondition;
 import rs.ltt.jmap.common.entity.filter.Filter;
 import rs.ltt.jmap.common.method.*;
@@ -41,6 +42,8 @@ import rs.ltt.jmap.common.method.response.identity.*;
 import rs.ltt.jmap.common.method.response.mailbox.ChangesMailboxMethodResponse;
 import rs.ltt.jmap.common.method.response.mailbox.GetMailboxMethodResponse;
 import rs.ltt.jmap.common.method.response.mailbox.SetMailboxMethodResponse;
+import rs.ltt.jmap.common.method.response.thread.ChangesThreadMethodResponse;
+import rs.ltt.jmap.common.method.response.thread.GetThreadMethodResponse;
 import rs.ltt.jmap.mock.server.Changes;
 import rs.ltt.jmap.mock.server.CreationIdResolver;
 import rs.ltt.jmap.mock.server.EmailGenerator;
@@ -659,15 +662,76 @@ public class Jmap {
   private MethodResponse[] execute(
       ChangesThreadMethodCall methodCall,
       ListMultimap<String, Response.Invocation> previousResponses) {
-    return new MethodResponse[] {new UnknownMethodMethodErrorResponse()};
-    // serve (sembra rompere starred, archive, drafts etc)
+    final String since = methodCall.getSinceState();
+    if (since != null && since.equals(getState())) {
+      return new MethodResponse[] {
+        ChangesThreadMethodResponse.builder()
+          .oldState(getState())
+          .newState(getState())
+          .updated(new String[0])
+          .created(new String[0])
+          .destroyed(new String[0])
+          .build()
+      };
+    } else {
+      final Update update = getAccumulatedUpdateSince(since);
+      if (update == null) {
+        return new MethodResponse[] {new CannotCalculateChangesMethodErrorResponse()};
+      } else {
+        final Changes changes = update.getChangesFor(Thread.class);
+        return new MethodResponse[] {
+          ChangesThreadMethodResponse.builder()
+            .oldState(since)
+            .newState(update.getNewVersion())
+            .updated(changes == null ? new String[0] : changes.updated)
+            .created(changes == null ? new String[0] : changes.created)
+            .destroyed(new String[0])
+            .hasMoreChanges(!update.getNewVersion().equals(getState()))
+            .build()
+        };
+      }
+    }
   }
 
   private MethodResponse[] execute(
       GetThreadMethodCall methodCall,
       ListMultimap<String, Response.Invocation> previousResponses) {
-    return new MethodResponse[] {new UnknownMethodMethodErrorResponse()};
-    // TODO: serve
+    final Request.Invocation.ResultReference idsReference = methodCall.getIdsReference();
+    final List<String> ids;
+    if (idsReference != null) {
+      try {
+        ids =
+          Arrays.asList(
+            ResultReferenceResolver.resolve(idsReference, previousResponses));
+      } catch (final IllegalArgumentException e) {
+        return new MethodResponse[] {new InvalidResultReferenceMethodErrorResponse()};
+      }
+    } else {
+      ids = Arrays.asList(methodCall.getIds());
+    }
+    final Thread[] threads =
+      ids.stream()
+        .map(
+          threadId ->
+            Thread.builder()
+              .id(threadId)
+              .emailIds(
+                getEmails().values().stream()
+                  .filter(
+                    email ->
+                      email.getThreadId()
+                        .equals(
+                          threadId))
+                  .sorted(
+                    Comparator.comparing(
+                      Email
+                        ::getReceivedAt))
+                  .map(Email::getId)
+                  .collect(Collectors.toList()))
+              .build())
+        .toArray(Thread[]::new);
+    return new MethodResponse[] {
+      GetThreadMethodResponse.builder().list(threads).state(getState()).build()
+    };
   }
-
 }
