a787a506706bf9d9eba1f004c766c42d713dc928
Francesco Furno <francesco.furno@studenti.unipd.it>
2024-02-20 09:47:42 +0100

    Aggiunta di UpdateDeserializer

---
+++ src/main/it/unipd/overture/business/UpdateDeserializer.java
@@ -0,0 +1,37 @@
+package it.unipd.overture.business;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+
+import rs.ltt.jmap.common.entity.AbstractIdentifiableEntity;
+import rs.ltt.jmap.mock.server.Update;
+
+import rs.ltt.jmap.mock.server.Changes;
+public class UpdateDeserializer implements JsonDeserializer<Update> {
+  /*
+  Gson gson;
+
+  UpdateDeserializer(Gson gson) {
+    this.gson = gson;
+  }
+
+  @Override
+  public Update deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+      throws JsonParseException {
+    Type changes_type = new TypeToken<Map<Class<? extends AbstractIdentifiableEntity>, Changes>>(){}.getType();
+    JsonObject jobject = json.getAsJsonObject();
+    return new Update(
+      gson.fromJson(gson.toJson(jobject.get("changes")), changes_type),
+      jobject.get("newVersion").getAsString()
+    );
+  }
+  */
+}
