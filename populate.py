e4c8f243102f7fbad7d537f3b33064b44307c1bb
Riccardo Fabbian <fabb.riccardo@gmail.com>
2024-02-23 11:24:43 +0100

    Aggiunta dello script per il popolamento del database

---
+++ populate.py
@@ -0,0 +1,35 @@
+import csv
+from rethinkdb import RethinkDB
+
+db_host = 'localhost'
+db_port = 28015
+db_db = 'jmap'
+domain = 'overture.duckdns.org'
+
+r = RethinkDB()
+r.connect(db_host, db_port, db=db_db).repl()
+
+try:
+    r.db_drop('jmap').run()
+except:
+    pass
+
+r.db_create('jmap').run()
+
+r.table_create('account').run()
+r.table('account').index_create('address')
+r.table_create('identity').run()
+r.table('identity').index_create('account').run()
+r.table_create('email').run()
+r.table_create('mailbox').run()
+# r.table('mailbox').index_create('account').run()
+r.table_create('update').run()
+
+with open('users.csv', mode='r') as f:
+    csv_f = csv.DictReader(f)
+    users = [{**x, 'state': 0} for x in csv_f]
+    userids = r.table('account').insert(users).run()['generated_keys']
+
+r.table('identity').insert(
+    [{'account': userids[i], 'name': users['username'], 'address': users['username']+'@'+domain} for i, u in enumerate(userids)]
+  ).run()
