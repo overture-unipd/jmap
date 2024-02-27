055703a770a69010bd4f66d0676294ddff5bb11d
francescobulychov <fc.bulychov@protonmail.com>
2024-02-27 18:23:11 +0100

    Aggiornato AttachmentHandler

---
+++ src/main/java/it/unipd/overture/business/AttachmentHandler.java
@@ -0,0 +1,30 @@
+package it.unipd.overture.business;
+
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import it.unipd.overture.ports.out.AttachmentPort;
+import rs.ltt.jmap.common.entity.Upload;
+
+public class AttachmentHandler {
+  Gson gson;
+  AttachmentPort attachmentPort;
+
+  @Inject
+  AttachmentHandler(Gson gson, AttachmentPort attachmentPort) {
+    this.gson = gson;
+    this.attachmentPort = attachmentPort;
+  }
+  
+  String upload(byte[] data) {
+    var blobid = attachmentPort.insertAttachment(data);
+    final Upload upload =
+      Upload.builder()
+        .blobId(blobid)
+        .build();
+      return gson.toJson(upload);
+  }
+
+  byte[] download(String id) {
+    return attachmentPort.getAttachment(id);
+  }
+}
