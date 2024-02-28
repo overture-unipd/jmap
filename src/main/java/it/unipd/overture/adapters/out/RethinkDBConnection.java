c27e800a10bbc60644f0fe350ec2e541a9e5b3c3
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2024-02-28 09:50:41 +0100

    Delete Configuration, and AttachmentRethinkDB. Move RethinkDBConnection

---
+++ src/main/java/it/unipd/overture/adapters/out/RethinkDBConnection.java
@@ -0,0 +1,20 @@
+package it.unipd.overture.adapters.out;
+
+import com.google.inject.Provides;
+import com.rethinkdb.RethinkDB;
+import com.rethinkdb.net.Connection;
+
+// Examples of driver usage: https://github.com/rethinkdb/rethinkdb-java/blob/master/src/test/java/com/rethinkdb/RethinkDBTest.java
+
+public class RethinkDBConnection {
+  private Connection conn;
+
+  public RethinkDBConnection(String host, Integer port, String db) {
+    this.conn = RethinkDB.r.connection().hostname(host).port(port).connect().use(db);
+  }
+
+  @Provides
+  public Connection provideConnection() {
+    return conn;
+  }
+}
