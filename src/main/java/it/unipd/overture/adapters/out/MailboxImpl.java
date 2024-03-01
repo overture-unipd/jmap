e8db9c4f1c534068186af324ee7a3819d933f744
AlexVedovato <alex.vedovato@gmail.com>
2024-03-01 18:17:33 +0100

    Adapters per: Mailbox, Thread, Update

--- src/main/java/it/unipd/overture/adapters/out/MailboxImpl.java
+++ src/main/java/it/unipd/overture/adapters/out/MailboxImpl.java
@@ -1,6 +1,45 @@
 package it.unipd.overture.adapters.out;
 
-import it.unipd.overture.ports.out.Mailbox;
+import java.util.List;
+import java.util.Map;
 
-public class Mailbox implements MailboxImpl {
+import com.google.inject.Inject;
+import com.rethinkdb.RethinkDB;
+import com.rethinkdb.net.Connection;
+
+import it.unipd.overture.ports.out.MailboxPort;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.rethinkdb.utils.Types;
+
+public class MailboxImpl implements MailboxPort {
+  private final RethinkDB r = RethinkDB.r;
+  private final TypeReference<Map<String, Object>> stringObjectMap = Types.mapOf(String.class, Object.class);
+  private Connection conn;
+
+  @Inject
+  MailboxImpl(Connection conn) {
+    this.conn = conn;
+  }
+
+  @Override
+  public String getMailbox(String id) {
+    return r.table("mailbox").get(id).toJson().run(conn).single().toString();
+  }
+
+  @Override
+  public String getAccountMailboxes(String id) {
+    return r.table("mailbox").getAll(id).optArg("index", "account").coerceTo("array").toJson().run(conn).single().toString();
+  }
+
+  @Override
+  public String insertMailbox(String mailbox) {
+    Map<String, Object> res = r.table("mailbox").insert(r.json(mailbox)).run(conn, stringObjectMap).single();
+    return ((List<?>) res.get("generated_keys")).get(0).toString();
+  }
+
+  @Override
+  public void deleteMailbox(String id) {
+    r.table("mailbox").get(id).delete().run(conn);
+  }
 }
--- src/main/java/it/unipd/overture/adapters/out/UpdateImpl.java
+++ src/main/java/it/unipd/overture/adapters/out/UpdateImpl.java
@@ -1,6 +1,32 @@
 package it.unipd.overture.adapters.out;
 
+import com.google.inject.Inject;
+import com.rethinkdb.RethinkDB;
+import com.rethinkdb.net.Connection;
+
 import it.unipd.overture.ports.out.UpdatePort;
 
 public class UpdateImpl implements UpdatePort {
+  private Connection conn;
+  private final RethinkDB r = RethinkDB.r;
+
+  @Inject
+  UpdateImpl(Connection conn) {
+    this.conn = conn;
+  }
+
+  @Override
+  public String insertUpdate(String update) {
+    return r.table("update").insert(update).run(conn).toString();
+  }
+
+  @Override
+  public String getUpdate(String id) {
+    return r.table("update").get(id).toJson().run(conn).single().toString();
+  }
+
+  @Override
+  public void deleteUpdate(String id) {
+    r.table("update").get(id).delete().run(conn);
+  }
 }
