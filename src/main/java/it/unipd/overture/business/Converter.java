b6552375cda147f240df3bf03606c1ceb7b73bc3
francescobulychov <fc.bulychov@protonmail.com>
2024-03-02 10:12:34 +0100

    Aggiornato Converter

--- src/main/java/it/unipd/overture/business/Converter.java
+++ src/main/java/it/unipd/overture/business/Converter.java
@@ -1,17 +1,22 @@
 package it.unipd.overture.business;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.inject.Provides;
+
+import rs.ltt.jmap.gson.JmapAdapters;
+
 public class Converter {
   private Gson gson;
 
   Converter() {
     var gsonBuilder = new GsonBuilder();
     JmapAdapters.register(gsonBuilder);
-    gson = gsonBuilder.create();
+    this.gson = gsonBuilder.create();
   }
 
-  String serialize(Object obj) {
-  }
-
-  String deserialize(Object obj) {
+  @Provides
+  public Gson provideGson() {
+    return gson;
   }
 }
