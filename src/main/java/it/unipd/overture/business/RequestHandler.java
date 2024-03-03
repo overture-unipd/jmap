f3818c6cf58279c65cd4126fb272d5185a8303ad
pampamxoxo <eleonoraamadori723@gmail.com>
2024-03-03 15:07:11 +0100

    Sistemato RequestHandler

--- src/main/java/it/unipd/overture/business/RequestHandler.java
+++ src/main/java/it/unipd/overture/business/RequestHandler.java
@@ -1,20 +1,56 @@
 package it.unipd.overture.business;
 
+import java.util.Base64;
+
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+
+import it.unipd.overture.ports.in.RequestPort;
+import it.unipd.overture.ports.out.AccountPort;
+import it.unipd.overture.ports.out.AttachmentPort;
+import rs.ltt.jmap.common.entity.Upload;
+
 public class RequestHandler implements RequestPort {
-  Dispatcher dispatcher;
+  AuthenticationHandler authentication;
+  SessionHandler session;
+  MethodHandler method;
+  AttachmentHandler attachment;
 
-  RequestHandler(Dispatcher dispatcher) {
+  @Inject
+  RequestHandler(
+      AuthenticationHandler authentication,
+      SessionHandler session,
+      MethodHandler method,
+      AttachmentHandler attachment
+  ) {
+    this.authentication = authentication;
+    this.session = session;
+    this.method = method;
+    this.attachment = attachment;
   }
 
-  String session(String json) {
+  @Override
+  public Boolean authenticate(String auth) {
+    return authentication.authenticate(auth);
   }
 
-  String postJmap(String id) {
+  @Override
+  public String session(String token) {
+    return session.get(token);
   }
 
-  byte[] download(String id) {
+  @Override
+  public String jmap(String json) {
+    return method.dispatch(json);
   }
 
-  void upload(Byte[] data) {
+  @Override
+  public String upload(byte[] data) {
+    return attachment.upload(data);
+  }
+
+  @Override
+  public byte[] download(String id) {
+    return attachment.download(id);
   }
 }
