62510536c969110055d1a443372b6e27a94d369d
AlexVedovato <alex.vedovato@gmail.com>
2024-03-03 08:09:00 +0100

    Rimossi Identity, Dispatcher ed Echo (vecchio). Aggiunto IdentityHandler

--- src/main/java/it/unipd/overture/business/Echo.java
+++
@@ -1,9 +0,0 @@
-package it.unipd.overture.business;
-
-public class Echo {
-  public echo(EchoMethodCall methodCall, ListMultimap<String, Response.Invocation> previousResponses) {
-    return new MethodResponse[] {
-      EchoMethodResponse.builder().libraryName(methodCall.getLibraryName()).build()
-    };
-  }
-}
---
+++ src/main/java/it/unipd/overture/business/IdentityHandler.java
@@ -0,0 +1,36 @@
+package it.unipd.overture.business;
+
+import com.google.common.collect.ListMultimap;
+
+import it.unipd.overture.ports.out.IdentityPort;
+import rs.ltt.jmap.common.Response;
+import rs.ltt.jmap.common.Response.Invocation;
+import rs.ltt.jmap.common.method.MethodResponse;
+import rs.ltt.jmap.common.method.call.identity.GetIdentityMethodCall;
+import rs.ltt.jmap.common.method.response.identity.GetIdentityMethodResponse;
+
+public class IdentityHandler {
+  IdentityPort identityPort;
+
+  IdentityHandler(IdentityPort identityPort) {
+    this.identityPort = identityPort;
+  }
+
+  public MethodResponse[] get(GetIdentityMethodCall methodCall, ListMultimap<String, Response.Invocation> previousResponses) {
+    return null;
+    /*
+    return new MethodResponse[] {
+      GetIdentityMethodResponse.builder()
+        .list(
+          new IdentityHandler[] {
+          IdentityHandler.builder()
+            .id(accountid)
+            .email(db.getAccountAddress(accountid))
+            .name(db.getAccountName(accountid))
+            .build()
+          })
+        .build()
+    };
+    */
+  }
+}
