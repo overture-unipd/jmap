3aea3a4db03589386d599522acb3024e7d1e6486
AlexVedovato <alex.vedovato@gmail.com>
2024-03-04 19:24:43 +0100

    Aggiornato Spark per usare nuove porte

--- src/main/java/it/unipd/overture/adapters/in/Spark.java
+++ src/main/java/it/unipd/overture/adapters/in/Spark.java
@@ -9,27 +9,47 @@ import static spark.Spark.post;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import com.google.inject.Inject;
+import it.unipd.overture.ports.in.AuthenticationPort;
+import it.unipd.overture.ports.in.SessionPort;
 import it.unipd.overture.ports.in.MethodPort;
+import it.unipd.overture.ports.in.UploadPort;
+import it.unipd.overture.ports.in.DownloadPort;
 import spark.Request;
 import spark.Response;
 import spark.utils.IOUtils;
 
 public class Spark {
-  private final MethodPort request;
+  private final AuthenticationPort authentication;
+  private final SessionPort session;
+  private final MethodPort method;
+  private final UploadPort upload;
+  private final DownloadPort download;
 
-  Spark(MethodPort request) {
-    this.request = request;
+  @Inject
+  Spark(
+      AuthenticationPort authentication,
+      SessionPort session,
+      MethodPort method,
+      UploadPort upload,
+      DownloadPort download
+  ) {
+    this.authentication = authentication;
+    this.session = session;
+    this.method = method;
+    this.upload = upload;
+    this.download = download;
   }
 
   private String authenticate(Request q, Response a) {
-    if (! request.authenticate(q.headers("Authorization"))) {
+    if (! authentication.authenticate(q.headers("Authorization"))) {
       halt(401, "Wrong credentials.");
     }
     return null;
   }
 
   private String up(Request q, Response a) {
-    return "Spark server is up";
+    return "Vivo, ma non ho scelta né un motivo.";
   }
 
   private String wellKnown(Request q, Response a) {
@@ -39,18 +59,18 @@ public class Spark {
 
   private String getJmap(Request q, Response a) {
     a.type("application/json");
-    return request.session(q.headers("Authorization"));
+    return session.get(q.headers("Authorization"));
   }
 
   private String postJmap(Request q, Response a) {
     a.type("application/json");
-    return request.jmap(q.body());
+    return method.get(q.body());
   }
 
   private String download(Request q, Response a) {
     a.type("application/octet-stream");
     var blobid = q.queryParams("blobid");
-    var blob = request.download(blobid);
+    var blob = download.pull(blobid);
     if (blob == null) {
       halt(500, "Something went wrong with the download.");
     }
@@ -69,10 +89,10 @@ public class Spark {
   private String upload(Request q, Response a) {
     a.type("application/json");
     var blob = q.bodyAsBytes();
-    return request.upload(blob);
+    return upload.push(blob);
   }
 
-  void start() {
+  public void start() {
     port(8000);
 
     before("/api/*", this::authenticate);
