6cb574d0af2acdfca876fb12105305ddec9a0192
Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>
2024-03-08 14:26:42 +0100

    Update .env and add jacoco to gradle build
    Signed-off-by: Riccardo Bonavigo <68009660+rickbonavigo@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 plugins {
     application
 
+    jacoco
+
     // id("com.diffplug.spotless") version "6.23.3"
     id("com.bmuschko.docker-java-application") version "9.4.0"
     id("com.dorongold.task-tree") version "2.1.1"
@@ -19,17 +21,21 @@ plugins {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
-    testImplementation("org.mockito:mockito-core:5.1.1")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.1.1")
+    testImplementation("org.mockito:mockito-core:5.9.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.9.0")
     testImplementation("org.testcontainers:testcontainers:1.19.3")
     testImplementation("org.testcontainers:junit-jupiter:1.19.3")
+    testImplementation("org.testcontainers:minio:1.19.7")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     implementation("com.google.guava:guava:32.1.1-jre")
 
     implementation("com.sparkjava:spark-core:2.9.4")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+    implementation("org.json:json:20240303")
+    implementation("org.slf4j:slf4j-api:2.0.12")
+    implementation("org.slf4j:slf4j-simple:2.0.12")
 
     implementation("com.rethinkdb:rethinkdb-driver:2.4.4")
     implementation("commons-logging:commons-logging:1.3.0")
@@ -41,7 +47,6 @@ dependencies {
     implementation("rs.ltt.jmap:jmap-mock-server:0.8.18")
 
     implementation("com.google.code.gson:gson:2.10.1")
-    // implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
 
     implementation("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
@@ -58,7 +63,7 @@ tasks.named<Test>("test") {
 }
 
 application {
-    mainClass.set("it.unipd.overture.jmap.Init")
+    mainClass.set("it.unipd.overture.Init")
 }
 
 docker {
@@ -67,10 +72,31 @@ docker {
         maintainer.set("Overture 'overture.unipd@gmail.com'")
         ports.set(listOf(8000))
         images.set(setOf("overture-unipd/jmap:latest"))
-        mainClassName.set("it.unipd.overture.jmap.Ignite")
+        mainClassName.set("it.unipd.overture.Init")
         // jvmArgs.set(listOf("-Xms256m", "-Xmx2048m"))
     }
 }
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        html.required.set(true)
+        xml.required.set(true)
+        csv.required.set(true)
+    }
+}
+/*
+jacoco {
+    applyTo(tasks.run.get())
+}
+tasks.register<JacocoReport>("applicationCodeCoverageReport") {
+    executionData(tasks.run.get())
+    sourceSets(sourceSets.main.get())
+}
+*/
 
 /*
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
